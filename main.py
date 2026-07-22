b32417637d1665112eaedc15b17c4fdb90044fd3
amirali11228jjgg-crypto <amirali11228jjgg@gmail.com>
2026-07-22 05:45:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters
+)
 
 from config import BOT_TOKEN
 from database import create_tables
@@ -11,13 +17,29 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "دستورات ربات:\n\n"
+        "/start - فعال بودن ربات\n"
+        "/help - راهنما"
+    )
+
+
+async def delete_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message and "http" in update.message.text:
+        await update.message.delete()
+
+
 def main():
     create_tables()
 
     app = Application.builder().token(BOT_TOKEN).build()
 
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+
     app.add_handler(
-        CommandHandler("start", start)
+        MessageHandler(filters.TEXT & ~filters.COMMAND, delete_links)
     )
 
     print("Bot is running...")
