2879fb5120d3f7f4aec7955620c4c82ad54bf9fc
amirali11228jjgg-crypto <amirali11228jjgg@gmail.com>
2026-07-23 00:35:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,95 @@
-print("TEST OK")
+import logging
+
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters
+)
+
+from config import BOT_TOKEN
+from database import init_db
+
+from commands import (
+    ban,
+    unban,
+    kick,
+    warn,
+    mute,
+    unmute,
+    warnings
+)
+
+from filters import link_filter
+
+
+logging.basicConfig(
+    level=logging.INFO
+)
+
+
+async def start(update, context):
+    await update.message.reply_text(
+        "🤖 ربات مدیریت گروه فعال شد."
+    )
+
+
+async def setup(application):
+    await init_db()
+
+
+def main():
+
+    app = (
+        Application.builder()
+        .token(BOT_TOKEN)
+        .post_init(setup)
+        .build()
+    )
+
+    app.add_handler(
+        CommandHandler("start", start)
+    )
+
+    app.add_handler(
+        CommandHandler("ban", ban)
+    )
+
+    app.add_handler(
+        CommandHandler("unban", unban)
+    )
+
+    app.add_handler(
+        CommandHandler("kick", kick)
+    )
+
+    app.add_handler(
+        CommandHandler("warn", warn)
+    )
+
+    app.add_handler(
+        CommandHandler("mute", mute)
+    )
+
+    app.add_handler(
+        CommandHandler("unmute", unmute)
+    )
+
+    app.add_handler(
+        CommandHandler("warnings", warnings)
+    )
+
+    app.add_handler(
+        MessageHandler(
+            filters.TEXT & ~filters.COMMAND,
+            link_filter
+        )
+    )
+
+    print("Bot started...")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
