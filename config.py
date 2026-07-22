7156937b6bba41286367aa95876800e0d57781d6
amirali11228jjgg-crypto <amirali11228jjgg@gmail.com>
2026-07-22 20:51:14 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,34 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-# خواندن متغیرهای محیطی
 load_dotenv()
 
-# توکن ربات تلگرام
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# تنظیمات اصلی ربات
+DATABASE_NAME = "database.db"
 
-# تعداد اخطار مجاز قبل از بن خودکار
-MAX_WARNINGS = 3
+MAX_WARNS = 3
 
-# دیتابیس
-DATABASE_NAME = "bot_database.db"
+MUTE_TIME = 3600
 
-# تنظیمات حذف لینک
-LINK_FILTER_ENABLED = True
+LINK_FILTER = True
 
-# تنظیمات لاگ
-LOG_ENABLED = True
-
-
-def get_config():
-    """
-    نمایش تنظیمات فعلی
-    """
-    return {
-        "max_warnings": MAX_WARNINGS,
-        "database": DATABASE_NAME,
-        "link_filter": LINK_FILTER_ENABLED,
-        "logging": LOG_ENABLED
-    }
+AUTO_BAN = True
